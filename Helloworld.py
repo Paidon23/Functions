305dd47ce3d11faec75ed7eeef2cdb353296a0d0
sacha <broute.sacha@gmail.com>
2014-10-28 16:54:00 +0100

    bla

--- Helloworld.py
+++ Helloworld.py
@@ -1,8 +1,4 @@
 ##Test
 
 def helloworld():
-<<<<<<< HEAD
-    print("Hello World. oooh mancala?")
-=======
-    print("Six fois sept")
->>>>>>> c2d03a2b42a9c20d7fbb309fb4f972c48ab30b48
+    print("blablabla")
